5449c618c0cb34bf9fdd048d0f4b0d4f2cb9c05a
Konrad Rulak <kornfix@outlook.com>
2022-07-27 20:36:21 +0200

    Test commit

---
+++ src/test/java/pl/university/b2c/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.university.b2c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
