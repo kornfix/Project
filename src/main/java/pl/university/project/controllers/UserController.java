392f8a621963d23948aa56a9455677eef6efd45d
Konrad Rulak <kornfix@outlook.com>
2022-08-14 17:37:09 +0200

    Dodanie logowanie i thymleaf

--- src/main/java/pl/university/project/controllers/UserController.java
+++ src/main/java/pl/university/project/controllers/UserController.java
@@ -1,29 +1,42 @@
 package pl.university.project.controllers;
 
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import pl.university.project.models.User;
 import pl.university.project.odata.UserData;
 import pl.university.project.services.UserService;
 
 import javax.annotation.Resource;
-import java.util.List;
+import java.util.Collections;
 
-@RestController
+@Controller
+@RequestMapping("/users")
 public class UserController {
 
 
     @Resource
     private UserService userService;
 
-    @GetMapping("/users")
-    public List<UserData> getAllUsers() {
-        return userService.getAllUsers();
+    @GetMapping
+    public String getAllUsers(Model model) {
+        UserData userData = new UserData();
+        userData.setId("12");
+        userData.setFirstName("Adam");
+        userData.setLastName("Nowak");
+        model.addAttribute("users", Collections.singleton(userData));
+        return "users";
     }
 
-    @GetMapping("/users/{id}")
-    public UserData getUserByID(@PathVariable String id) {
-        return userService.getUserById(id);
+    @GetMapping("/{id}")
+    public String getUserByID(@PathVariable String id, Model model) {
+        UserData userData = new UserData();
+        userData.setId("12");
+        userData.setFirstName("Adam");
+        userData.setLastName("Nowak");
+        model.addAttribute("user", Collections.singleton(userData));
+        return "user";
     }
 
     @PostMapping(value = "/users", consumes = MediaType.APPLICATION_JSON_VALUE)
