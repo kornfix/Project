90cfd54c09e16dd9b4a5573a07728f8f7fdb413b
Konrad Rulak <kornfix@outlook.com>
2022-08-15 19:12:50 +0200

    Naprawienie usuwania, dodanie redirecta na error i notfound kiedy nie istnieje strona

--- src/main/java/pl/university/project/configs/MvcConfig.java
+++ src/main/java/pl/university/project/configs/MvcConfig.java
@@ -1,7 +1,12 @@
 package pl.university.project.configs;
 
 
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,6 +20,16 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/users").setViewName("users");
         registry.addViewController("/clients").setViewName("clients");
+        registry.addViewController("/error").setViewName("error");
+        registry.addViewController("/notFound").setViewName("notFound");
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> containerCustomizer() {
+        return container -> {
+            container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND,
+                    "/notFound"));
+        };
     }
 
 }
